9341b7cbef56040af1ef1c8a858cd90214b61a48
Ansari s <ansaris@vdartinc.com>
2025-02-08 23:46:40 +0530

    add
    Signed-off-by: Ansari s <ansaris@vdartinc.com>

--- src/login2/SignInSide.tsx
+++ src/login2/SignInSide.tsx
@@ -32,11 +32,22 @@ export default function SignInSide(props: { disableCustomTheme?: boolean }) {
               backgroundImage:
                 'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
               backgroundRepeat: 'no-repeat',
+              // height: '132%',
+                '@media screen and (max-width: 900px) ': {
+                  height: '170%',
+                },
               ...theme.applyStyles('dark', {
                 backgroundColor: 'hsl(220, 30%, 5%)',
                 backgroundImage:
                   'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
-                  height: '132%',
+                  // height: '132%',
+                  
+                '@media screen and (max-width: 1300px) and (min-width: 900px) ': {
+                 height: '132%',
+                },
+                '@media screen and (max-width: 1550px) and (min-width: 1300px) ': {
+                 height: '112%',
+                },
                 '@media screen and (max-width: 900px) ': {
                   height: '170%',
                 },
